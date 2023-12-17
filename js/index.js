26d8bf87181a927b4246977e6dd2928f5c461991
brandon arias <abrandon.a7@gmail.com>
2023-12-17 20:26:24 -0300

    segunda preentrega

--- js/index.js
+++ js/index.js
@@ -1,129 +1,247 @@
-function validacionRespuesta(pregunta, respuestaCorrecta){
-    let respuesta = prompt(pregunta).toUpperCase();
+// funciones globales
+const agregarProducto = (array, producto) => {
+    array.push(producto);
+    productID++;
+};
 
-    while(respuesta !== "A" && respuesta !== "B" && respuesta !== "C"){
-        alert("Respuesta invalida responder por A, B o C");
-        respuesta = prompt(pregunta).toUpperCase();
+const validacionDeExistencia = (array, idProducto) => {
+    let existe = array.some(elemento => elemento.id === idProducto);
+    return existe;
+}
+
+
+const eliminarProducto = (array, idPr, categoria) => {
+    if (validacionDeExistencia(array, idPr)) {
+        categoria = array.filter(producto => producto.id !== idPr);
+    } else{
+        alert ("No existe ningun producto con ese ID.");
     }
+};
 
-    if(respuesta === respuestaCorrecta){
-        alert("Respuesta correcta.");
-    } else {
-        alert("Respuesta incorrecta");
-    }   
+const mostrarProductos = (array) => {
+    let nombreCategoria = array.map(producto => `ID: ${producto.id} - Nombre: ${producto.nombreProducto}`);
+
+    if(nombreCategoria.length > 0){
+        alert(nombreCategoria.join("\n"));
+    }else {
+        alert("No hay productos cargados.")
+    }
+};
+
+let productID = 1;
+let tabacos = [];
+let papelillos = [];
+let filtros = [];
+let accesorios = [];
+
+class Producto {
+    constructor(nombreProducto, precioProducto){
+        this.nombreProducto = nombreProducto;
+        this.precioProducto = precioProducto;
+        this.id = productID;
+    }
 }
 
-let nombre = prompt("Ingrese su nombre:").toUpperCase();
-alert(`Bienvenido ${nombre} al trivia de preguntas!!`);
+// menu tabacos
+const menuTabacos= () => {
+    let estado1 = true;
 
-let tematica = prompt("Elija una tematica:(responder por A, B o C) \nA. Matematica\nB. Historia\nC .Geografia").toUpperCase();
-
-while(tematica !== "A" && tematica !== "B" && tematica !== "C"){
-    alert("Respuesta invalida");
-    tematica = prompt("Elija una tematica:(responder por A, B o C) \nA. Matematica\nB. Historia\nC .Geografia").toUpperCase();
+    while(estado1){
+        let opcion = parseInt(prompt(
+            `
+            Seleccione una accion:
+            1- Agregar producto.
+            2- Eliminar producto.
+            3- Mostrar productos.
+            4- Volver al menu de categorias.
+            `
+        ));
+        switch (opcion) {
+            case 1:
+                let nombreProducto = prompt("Ingrese el nombre del producto.");
+                let precioProducto = prompt("Ingrese el precio del producto."); 
+                const producto = new Producto(nombreProducto, precioProducto);
+                agregarProducto(tabacos, producto);  
+                break;
+            case 2:
+                let idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar."));
+                eliminarProducto(tabacos, idProducto, tabacos);
+                break;
+            case 3: 
+                mostrarProductos(tabacos); 
+                break;
+            case 4:
+                estado1 = false;
+                menuAdmin();
+                break;    
+            default:
+                alert("Ingrese una opcion valida.");
+                break;
+        }  
+    } 
 }
 
-if(tematica === "A"){
+//menu papelillos
+const menuPapelillos= () => {
+    let estado = true;
+
+    while(estado){
+        let opcion = parseInt(prompt(
+            `
+            Seleccione una accion:
+            1- Agregar producto.
+            2- Eliminar producto.
+            3- Mostrar productos.
+            4- Volver al menu de categorias.
+            `
+        ));
+        switch (opcion) {
+            case 1:
+                let nombreProducto = prompt("Ingrese el nombre del producto.");
+                let precioProducto = prompt("Ingrese el precio del producto."); 
+                const producto = new Producto(nombreProducto, precioProducto);
+                agregarProducto(papelillos, producto);
+                break;
+            case 2:
+                let idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar."));
+                eliminarProducto(papelillos, idProducto, papelillos);
+                break;
+            case 3:
+                mostrarProductos(papelillos); 
+                break;
+            case 4:
+                estado = false;
+                menuAdmin();
+                break;    
+            default:
+                alert("Ingrese una opcion valida.");
+                break;
+        }
+    }
     
-    for (let i = 0; i <= 4; i++) {
-        
-        let pregunta;
-        let respuestaCorrecta;
+}
 
-        switch(i) {
+//menu filtros
+const menuFiltros= () => {
+    let estado = true;
+
+    while(estado){
+        let opcion = parseInt(prompt(
+            `
+            Seleccione una accion:
+            1- Agregar producto.
+            2- Eliminar producto.
+            3- Mostrar productos.
+            4- Volver al menu de categorias.
+            `
+        ));
+        switch (opcion) {
             case 1:
-                pregunta = "¿Cuánto es el valor de pi?(responder por A, B o C): \nA. 1.88 \nB. 3.14 \nC. 3.17";
-                respuestaCorrecta = "B";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                let nombreProducto = prompt("Ingrese el nombre del producto.");
+                let precioProducto = prompt("Ingrese el precio del producto."); 
+                const producto = new Producto(nombreProducto, precioProducto);
+                agregarProducto(filtros, producto);
                 break;
             case 2:
-                pregunta = "¿Cuánto es el resultado de: 132 + 17 x 2?(responder por A, B o C): \nA. 166 \nB. 298 \nC. 281";
-                respuestaCorrecta = "A";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                let idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar."));
+                eliminarProducto(filtros, idProducto, filtros);
                 break;
             case 3:
-                pregunta = "¿Cuánto es el valor de 12^2?(responder por A, B o C): \nA. 144 \nB. 122 \nC. 224";
-                respuestaCorrecta = "A";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                mostrarProductos(filtros); 
                 break;
             case 4:
-                pregunta = "¿Cuántos lados tiene un eneagono?(responder por A, B o C): \nA. 7 \nB. 11 \nC. 9";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
+                estado = false;
+                menuAdmin();
+                break;    
             default:
-                alert("Le realizaremos 4 preguntas de Matematicas.");
+                alert("Ingrese una opcion valida.");
                 break;
         }
     }
+    
+}
 
-}else if (tematica === "B"){
+//menu accesorios
+const menuAccesorios= () => {
+    
+    let estado = true;
 
-    for (let i = 0; i <= 4; i++) {
-        let pregunta;
-        let respuestaCorrecta;
-
-        switch(i) {
+    while(estado){
+        let opcion = parseInt(prompt(
+            `
+            Seleccione una accion:
+            1- Agregar producto.
+            2- Eliminar producto.
+            3- Mostrar productos.
+            4- Volver al menu de categorias.
+            `
+        ));
+        switch (opcion) {
             case 1:
-                pregunta = "¿En que año se descubrio America?(responder por A, B o C): \nA.1495  \nB. 1485 \nC. 1492";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                let nombreProducto = prompt("Ingrese el nombre del producto.");
+                let precioProducto = prompt("Ingrese el precio del producto."); 
+                const producto = new Producto(nombreProducto, precioProducto);
+                agregarProducto(accesorios, producto);
                 break;
             case 2:
-                pregunta = "¿Cuando comenzo la primera guerra mundial?(responder por A, B o C): \nA. 1914 \nB. 1918 \nC. 1945";
-                respuestaCorrecta = "A";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                let idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar."));
+                eliminarProducto(accesorios, idProducto, accesorios);
                 break;
             case 3:
-                pregunta = "¿Cuando comenzo la revolucion francesa?(responder por A, B o C): \nA. 1799 \nB. 1786 \nC. 1789";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
+                mostrarProductos(accesorios); 
                 break;
             case 4:
-                pregunta = "¿En que año se independizo la Argentina?(responder por A, B o C): \nA. 1820 \nB. 1810 \nC. 1816";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
+                estado = false;
+                menuAdmin();
+                break;    
             default:
-                alert("Le realizaremos 4 preguntas de Historia.");
-                break;
-        }
-    }
-
-}else if (tematica === "C"){
-
-    for (let i = 0; i <= 4; i++) {
-        let pregunta;
-        let respuestaCorrecta;
-
-        switch(i) {
-            case 1:
-                pregunta = "¿Cual es la capital de Noruega?(responder por A, B o C): \nA. Dublin \nB. Ottawa \nC. Oslo";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
-            case 2:
-                pregunta = "¿Que paises limitan con Argentina?(responder por A, B o C): \nA. Chile, Brasil, Colombia, Uruguay y Paraguay \nB. Brasil, Bolivia, Chile, Paraguay y Uruguay \nC. Brasil, Uruguay, Bolivia, Ecuador y Paraguay";
-                respuestaCorrecta = "B";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
-            case 3:
-                pregunta = "¿En que continente se encuentra Uzbekistan?(responder por A, B o C): \nA. Asia \nB. Europa \nC. Oceania";
-                respuestaCorrecta = "A";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
-            case 4:
-                pregunta = "¿Cuantos continentes existen actualmente?(responder por A, B o C): \nA. 5 \nB. 6 \nC. 7";
-                respuestaCorrecta = "C";
-                validacionRespuesta(pregunta, respuestaCorrecta);
-                break;
-            default:
-                alert("Le realizaremos 4 preguntas de Geografia.");
+                alert("Ingrese una opcion valida.");
                 break;
         }
     }
 }
 
-alert("Gracias por participar de la trivia.");
+// Seccion de opciones de administrador
 
+const menuAdmin = () => {
+    let estado = true;
+    while (estado) {
+        let opcion = parseInt(
+            prompt(`
+            Bienvenido al menu de control de productos.
+            Seleccione una categoria:
+            1- Tabacos.
+            2- Papelillos.
+            3- Filtros.
+            4- Accesorios.
+            5- Salir. 
+            `)
+        );
+        switch (opcion) {
+            case 1:
+                //seccion tabacos
+                menuTabacos();
+                break;   
+            case 2:
+                //seccion papelillos
+                menuPapelillos();
+                break;
+            case 3:
+                //seccion filtros
+                menuFiltros();
+                break;
+            case 4:
+                //seccion accesorios
+                menuAccesorios();
+                break;
+            case 5:
+                estado = false;
+                break;
+            default:
+                alert("Por favor ingrese una opción válida.");
+                break;
+        }
+    }
+}
 
+menuAdmin();
